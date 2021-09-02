c3ac992b8fbbb4d20b3f1b82ab8f7b37f6f47af8
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-09-02 18:22:59 +0300

    adjusted 'game over' sign and button positions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 import Fruit from './fruit.js';
 import Matrix from './matrix.js';
 import Snake from './snake.js';
-import Utility from './random.js'
+import Utility from './random.js';
 
 window.onload = function (e) {
   const field = document.querySelector('.fields');
   const scoreField = document.getElementById('score');
   const restartBtn = document.getElementById('restart');
   const gameOver = document.querySelector('.game-over-container');
-  const btnContainer = document.querySelector('.btn-container');
-  
-  
 
   // set board dimensions and cell size
   const boardDimensions = {
     x: 17,
     y: 17,
-    cellSize: 40
-  }
+    cellSize: 40,
+  };
 
   // snake starting position
-  const snakeStartPosition = [[8, 9], [7, 9]];
+  const snakeStartPosition = [
+    [8, 9],
+    [7, 9],
+  ];
 
   // create gameboard(matrix) and snake objects
   const matrix = new Matrix(field, boardDimensions);
@@ -32,19 +32,19 @@ window.onload = function (e) {
   snake.initInputListener();
 
   // create first fruit
-  (new Fruit(matrix, Utility.generateFruit(matrix, snake))).show();
+  new Fruit(matrix, Utility.generateFruit(matrix, snake)).show();
 
   // game score
   let score = 0;
 
   // game loop
   const gameLoop = setInterval(() => {
-    if(!snake.snekDead) {
-      if(snake.ateFruit) {
+    if (!snake.snekDead) {
+      if (snake.ateFruit) {
         score++;
         snake.ateFruit = false;
 
-        (new Fruit(matrix, Utility.generateFruit(matrix, snake))).show();
+        new Fruit(matrix, Utility.generateFruit(matrix, snake)).show();
       }
 
       scoreField.textContent = score;
@@ -52,11 +52,10 @@ window.onload = function (e) {
     } else {
       clearInterval(gameLoop);
       gameOver.classList.remove('hidden');
-      btnContainer.classList.remove('hidden');
     }
   }, 500);
 
-  restartBtn.addEventListener('click', (e) => {
+  restartBtn.addEventListener('click', e => {
     location.reload();
-  })
-}
+  });
+};
